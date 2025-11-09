8bd1a9c618161161e8b199d9b63a89914b810769
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2025-11-10 01:47:17 +0200

    take a concept of aggregate functions Count and Sum (16 to 19)

---
+++ ExpensesRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YouTubeLINQ
+{
+    public record Expenses(
+        DateOnly Date,
+        double Amount,
+        ExpensesType Type
+    );
+
+    public static class ExpensesRepository
+    {
+        public static IEnumerable<Expenses> GetExpenses()
+        {
+            return new List<Expenses>
+            {
+                new(new DateOnly(2024, 12, 1), 10.85, ExpensesType.Outcome),
+                new(new DateOnly(2024, 12, 2), 88.17, ExpensesType.Income),
+                new(new DateOnly(2024, 12, 3), 35.8,  ExpensesType.Income),
+                new(new DateOnly(2024, 12, 4), 58.6,  ExpensesType.Outcome),
+                new(new DateOnly(2024, 12, 6), 46.19, ExpensesType.Outcome),
+                new(new DateOnly(2024, 12, 7), 91.91, ExpensesType.Income),
+                new(new DateOnly(2024, 12, 8), 32.59, ExpensesType.Income),
+                new(new DateOnly(2024, 12, 9), 75.7,  ExpensesType.Outcome),
+                new(new DateOnly(2024, 12, 10), 68.97, ExpensesType.Income),
+                new(new DateOnly(2024, 12, 11), 75.28, ExpensesType.Outcome),
+                new(new DateOnly(2024, 12, 12), 3.88,  ExpensesType.Income),
+                new(new DateOnly(2024, 12, 13), 12.37, ExpensesType.Outcome),
+                new(new DateOnly(2024, 12, 14), 83.76, ExpensesType.Income),
+                new(new DateOnly(2024, 12, 15), 32.91, ExpensesType.Outcome)
+            };
+        }
+
+        public static void PrintDate(IEnumerable<Expenses> expenses)
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("|    Date    |   Amount  |    Type    |");
+            Console.WriteLine("---------------------------------------");
+
+            foreach (var expense in expenses)
+            {
+                Console.WriteLine($"| {expense.Date:MM/dd/yyyy} | {expense.Amount,-9:F2} | {expense.Type,-10} |");
+            }
+
+            Console.WriteLine("---------------------------------------");
+        }
+    }
+
+    public enum ExpensesType
+    {
+        Income,
+        Outcome
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using LearnLINQ;
 using LinqForCSharpDevelopers;
+using YouTubeLINQ;
 
 var cars = CarRepository.GetCars();
 
@@ -233,5 +234,93 @@ var cars = CarRepository.GetCars();
 //var result = cars.All(c => c.ManufactureYear > 2000);
 //Console.WriteLine(result ? "all cars are new" : "not all cars are new");
 
+
+#endregion
+
+
+#region #9 append() and prepend()
+
+//var EndCar = new Car(Id: 1001, Make: "Tesla", Model: "Model S", ManufactureYear: 2022, VIN: "5YJSA1E26JF256789", Color: "White", MaxSpeed: 250);
+//var FirstCar = new Car(Id: 0, Make: "BMW", Model: "i8", ManufactureYear: 2021, VIN: "WBY2Z6C57JV123456", Color: "Black", MaxSpeed: 240);
+
+//var result = cars.Append(EndCar);
+
+//result = result.Prepend(FirstCar);
+//CarRepository.PrintCars(result);
+
+
+#endregion
+
+#region #10 using Count and logCount and CountBy
+
+//var totalCount = cars.Count();
+//var totalFordCarCount = cars.Count(c => c.Make == "Ford");
+
+////will return exception if value greater than int 32 maximal value
+//Console.WriteLine($"Total cars count: {totalCount}");
+//Console.WriteLine($"Total Ford cars count: {totalFordCarCount}");
+
+////will return exception if value greater than int 64 maximal value
+//var totalLongCount = cars.LongCount();
+//Console.WriteLine($"Total cars count using LongCount: {totalLongCount}");
+
+//count by new in .NET 9
+//var countByMake = cars.CountBy(c => c.Make).OrderByDescending(cars => cars.Value);
+//foreach (var kvp in countByMake)
+//{
+//    Console.WriteLine($"{kvp.Key} --> {kvp.Value}");
+//}
+
+
+//var countByMake = cars.CountBy(c => $"{c.Make}-{c.Color}").OrderBy(cars => cars.Key);
+//foreach (var kvp in countByMake)
+//{
+//    Console.WriteLine($"{kvp.Key} --> {kvp.Value}");
+//}
+
+#endregion
+
+
+#region #11 SUM
+
+//double[] numbers = [5.7, 10.2, 15, 20, 25];
+//var result = numbers.Sum();
+//Console.WriteLine($"The sum is: {result}");
+
+var expenses = ExpensesRepository.GetExpenses();
+
+
+var totalIncome = expenses
+    .Where(e => e.Type == ExpensesType.Income)
+    .Sum(e => e.Amount);//select the Amount property and sum it
+
+
+
+var totalOutcome = expenses
+    .Where(e => e.Type == ExpensesType.Outcome)
+    .Sum (e => e.Amount);
+
+ExpensesRepository.PrintDate(expenses);
+Console.WriteLine($"Total Income: {totalIncome:F2}");
+Console.WriteLine($"Total Outcome: {totalOutcome:F2}");
+
+
+/* concept of interpolated strings
+ double value = 1234.5678;
+
+Console.WriteLine($"F2: {value:F2}");//fixed-point with 2 decimal places
+Console.WriteLine($"N2: {value:N2}");//number with 2 decimal places and thousands separator
+Console.WriteLine($"C2: {value:C2}");//currency with 2 decimal places
+Console.WriteLine($"P2: {0.1234:P2}");//percentage with 2 decimal places
+Console.WriteLine($"E2: {value:E2}");//scientific notation with 2 decimal places
+
+ //output:
+F2: 1234.57
+N2: 1,234.57
+C2: $1,234.57
+P2: 12.34%
+E2: 1.23E+03
+
+ */
 
 #endregion
